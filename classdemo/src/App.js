a933577058342777ac1119c04b9f67289ebbc39f
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-10-24 06:03:16 +0000

    add stateful component

--- classdemo/src/App.js
+++ classdemo/src/App.js
@@ -1,22 +1,23 @@
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header 
+        linkText="Learn React"
+        linkUrl="https://reactjs.org"
+     />
     </div>
   );
 }
 
 export default App;
+
+
+// props -> passed down from parent to child. A child shouldn't change the props f(x, y, z)
+
+// state -> is owned by component, f has its own state, this.state.g
+
+// setState() function -> component lifecycle methods
+
+// hooks -> useeffects
