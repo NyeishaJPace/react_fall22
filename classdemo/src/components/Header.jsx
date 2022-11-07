16c4d8d5e539f4efd672a3d35a8645539174b756
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-11-07 01:20:59 +0000

    moved text

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -24,31 +24,13 @@ class Header extends Component{
         // this.props.linkText
         return(
             <header className={`App-header ${this.state.theme}`}>
-                <h1 style={{fontSize: '40px',}}>Nyeisha Pace  <br></br> IT Student Intern  <br></br> Hurtsboro,AL</h1>
-                <h2>About</h2>
-                <p> 
-                 I'm a IT student intern currently working with the Bureau of Engraving and Printing.
-                 <br></br>I am currently completing coursework at CSU and i have 3 years of experience in IT.
+                <h1 style={{fontSize: '40px',}}>Nyeisha Pace  <br></br> IT Student Intern  <br></br> Hurtsboro, AL</h1>
                 
-                </p>
+                
                 <a className="App-link" href={linkUrl} target="_blank" rel="noopener noreferrer" >
                     {linkText}
                 </a>
-                <h3>Work</h3>
-                <p> 
-                 Information Technology Student Intern
                 
-                </p>
-                
-                <h4>Design</h4>
-                {/*<img src="Design1.png" alt="Design"/>*/}
-                
-
-                
-
-                {/*<button onClick={() => this.setState({ count : this.state.count +1})}>
-                    Increment Count
-        </button>*/}
                 <MainBody />
             </header>
         )
--- classdemo/src/components/MainBody.jsx
+++ classdemo/src/components/MainBody.jsx
@@ -10,6 +10,36 @@ function MainBody(){
 
     return(
         <div>
+            <h2> About</h2>
+                <p>
+                    I'm a IT student intern currently working with the Bureau of Engraving and Printing.
+                    <br></br>
+                    I am currently completing coursework at CSU and i have 3 years of experience in IT.
+                    <br></br>
+                    I have an interest in building web applications, software and computer installations and upgrades.
+                 
+                 {/*I'm a IT student intern currently working with the Bureau of Engraving and Printing.
+                 <br></br>I am currently completing coursework at CSU and i have 3 years of experience in IT.<br></br>
+        I have an interest in building web applications, software and computer installations and upgrades.*/}
+        
+                </p>
+               
+                <h3>Work</h3>
+                <p> 
+                 Information Technology Student Intern
+                
+                </p>
+                
+                <h4>Design</h4>
+                {/*<img src="Design1.png" alt="Design"/>*/}
+                
+
+                
+
+                {/*<button onClick={() => this.setState({ count : this.state.count +1})}>
+                    Increment Count
+        </button>*/}
+        
             <h3> Your name is: <i>{name}</i></h3>
             <input type="text" value={name} onChange={handleChange} />
         </div>
