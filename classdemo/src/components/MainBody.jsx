ca8e9a60aaf4cade8fb45411859951f1c782c140
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-11-07 04:22:51 +0000

    updated img

--- classdemo/src/components/MainBody.jsx
+++ classdemo/src/components/MainBody.jsx
@@ -1,5 +1,6 @@
 // create a functional component with hooks
 import React, { useState } from 'react';
+//import Design from '/images/Design1.png';
 
 function MainBody(){
     const [name, setName] = useState('')
@@ -18,10 +19,6 @@ function MainBody(){
                     <br></br>
                     I have an interest in building web applications, software and computer installations and upgrades.
                  
-                 {/*I'm a IT student intern currently working with the Bureau of Engraving and Printing.
-                 <br></br>I am currently completing coursework at CSU and i have 3 years of experience in IT.<br></br>
-        I have an interest in building web applications, software and computer installations and upgrades.*/}
-        
                 </p>
                
                 <h3>Work</h3>
